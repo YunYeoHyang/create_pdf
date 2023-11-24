a1e8afd8c2e89602fb21aabc5c9a00c6f24b3404
曾令梓 <17683996041@163.com>
2023-11-24 18:08:48 +0800

    update test version

--- tools/dynamic_paragraph_gen.py
+++ tools/dynamic_paragraph_gen.py
@@ -34,40 +34,40 @@ def table_order_paragraph_gen(i, row_number, data, table, j, page, is_first, bor
 
     if is_first:
         table.add(get_table_cell_order_index("", "Helvetica", 1, 1, True, False))
-        table.add(get_table_cell_order_info1(
+        table.add(get_table_cell_order_info(
             f"{j['pkgs']} PKGS", "Helvetica", 8, 1, True, False, True, True, Alignment.RIGHT))
-        table.add(get_table_cell_order_info1("", "Helvetica", 2, 1, True, False, True, True, Alignment.LEFT))
-        table.add(get_table_cell_order_info1("", "Helvetica", 3, 1, True, False, True, True, Alignment.LEFT))
-        table.add(get_table_cell_order_info1("", "Helvetica", 2, 1, True, False, True, True, Alignment.LEFT))
+        table.add(get_table_cell_order_info("", "Helvetica", 2, 1, True, False, True, True, Alignment.LEFT))
+        table.add(get_table_cell_order_info("", "Helvetica", 3, 1, True, False, True, True, Alignment.LEFT))
+        table.add(get_table_cell_order_info("", "Helvetica", 2, 1, True, False, True, True, Alignment.LEFT))
         table.add(get_table_cell_order_index(f"00{i + 1}", "Helvetica", 1, row_number - 1, False, False))
     else:
         table.add(get_table_cell_order_index(f"00{i + 1}", "Helvetica", 1, row_number, False, False))
 
     for index in range(len(info_list)):
-        table.add(get_table_cell_order_info1(
+        table.add(get_table_cell_order_info(
             f"{info_list[index]['infoField1']}", "Helvetica", 8, 1, False, False, True, True, Alignment.LEFT))
-        table.add(get_table_cell_order_info1("", "Helvetica", 2, 1, False, False, True, True, Alignment.LEFT))
-        table.add(get_table_cell_order_info1("", "Helvetica", 3, 1, False, False, True, True, Alignment.LEFT))
-        table.add(get_table_cell_order_info1("", "Helvetica", 2, 1, False, False, True, True, Alignment.LEFT))
+        table.add(get_table_cell_order_info("", "Helvetica", 2, 1, False, False, True, True, Alignment.LEFT))
+        table.add(get_table_cell_order_info("", "Helvetica", 3, 1, False, False, True, True, Alignment.LEFT))
+        table.add(get_table_cell_order_info("", "Helvetica", 2, 1, False, False, True, True, Alignment.LEFT))
 
-        table.add(get_table_cell_order_info1(
+        table.add(get_table_cell_order_info(
             f"{info_list[index]['infoField2']}", "Helvetica", 1, 1, False, False, False, True, Alignment.LEFT))
-        table.add(get_table_cell_order_info1(
+        table.add(get_table_cell_order_info(
             f"{add_commas(trans_json_for_key(info_list[index], 'infoField3'))} KG" if
             trans_json_for_key(info_list[index], 'infoField3') != "" else "",
             "Helvetica", 4, 1, False, False, False, False, Alignment.RIGHT))
 
         if index == len(info_list) - 1:
-            table.add(get_table_cell_order_info1(
+            table.add(get_table_cell_order_info(
                 f"{data['pd1']['manifestQty']} NO" if data['pd1']['manifestQtyShow'] else "",
                 "Helvetica", 3, 1, False, False, True, False, Alignment.RIGHT))
-            table.add(get_table_cell_order_info1(f"${data['pd2']['totalValue']}",
-                                                 "Helvetica", 2, 1, False, False, True, True, Alignment.RIGHT))
+            table.add(get_table_cell_order_info(f"${data['pd2']['totalValue']}",
+                                                "Helvetica", 2, 1, False, False, True, True, Alignment.RIGHT))
         else:
-            table.add(get_table_cell_order_info1(
+            table.add(get_table_cell_order_info(
                 "",
                 "Helvetica", 3, 1, False, False, True, False, Alignment.RIGHT))
-            table.add(get_table_cell_order_info1("", "Helvetica", 2, 1, False, False, True, True, Alignment.LEFT))
+            table.add(get_table_cell_order_info("", "Helvetica", 2, 1, False, False, True, True, Alignment.LEFT))
 
         string = f"{trans_json_for_key(data['pd3'], 'htsusRate' + f'{index + 1}')}" if \
             trans_json_for_key(data['pd3'], 'htsusRate' + f'{index + 1}') else ""
@@ -76,49 +76,49 @@ def table_order_paragraph_gen(i, row_number, data, table, j, page, is_first, bor
                       f"{data['pd3']['valuedOverUnit']}\t+\t")
             # todo
             # f"{trans_json_for_key(data['pd3'], 'htsusRate' + f'{index + 1}')}")
-        table.add(get_table_cell_order_info1(
+        table.add(get_table_cell_order_info(
             string,
             "Helvetica", 3, 1, False, False, True, True, Alignment.LEFT))
-        table.add(get_table_cell_order_info1(
+        table.add(get_table_cell_order_info(
             f"${data['pd4']['dutyAndIrTax' + f'{index + 1}']}",
             "Helvetica", 2, 1, False, False, True, True, Alignment.RIGHT))
 
-    table.add(get_table_cell_order_info1(
+    table.add(get_table_cell_order_info(
         f"{data['pd1']['netQuantityIn']} KG" if data['pd1']['netQuantityInShow'] else "",
         "Helvetica", 8, 1, False, False, True, True, Alignment.RIGHT))
-    table.add(get_table_cell_order_info1(f"C ${data['pd2']['moneyC']}",
-                                         "Helvetica", 2, 1, False, False, True, True, Alignment.RIGHT))
-    table.add(get_table_cell_order_info1("", "Helvetica", 3, 1, False, False, True, True, Alignment.LEFT))
-    table.add(get_table_cell_order_info1("", "Helvetica", 2, 1, False, False, True, True, Alignment.LEFT))
+    table.add(get_table_cell_order_info(f"C ${data['pd2']['moneyC']}",
+                                        "Helvetica", 2, 1, False, False, True, True, Alignment.RIGHT))
+    table.add(get_table_cell_order_info("", "Helvetica", 3, 1, False, False, True, True, Alignment.LEFT))
+    table.add(get_table_cell_order_info("", "Helvetica", 2, 1, False, False, True, True, Alignment.LEFT))
 
-    table.add(get_table_cell_order_info1("", "Helvetica", 8, 1, False, False, True, True, Alignment.RIGHT))
-    table.add(get_table_cell_order_info1("N", "Helvetica", 2, 1, False, False, True, True, Alignment.RIGHT))
-    table.add(get_table_cell_order_info1("", "Helvetica", 3, 1, False, False, True, True, Alignment.LEFT))
-    table.add(get_table_cell_order_info1("", "Helvetica", 2, 1, False, False, True, True, Alignment.LEFT))
+    table.add(get_table_cell_order_info("", "Helvetica", 8, 1, False, False, True, True, Alignment.RIGHT))
+    table.add(get_table_cell_order_info("N", "Helvetica", 2, 1, False, False, True, True, Alignment.RIGHT))
+    table.add(get_table_cell_order_info("", "Helvetica", 3, 1, False, False, True, True, Alignment.LEFT))
+    table.add(get_table_cell_order_info("", "Helvetica", 2, 1, False, False, True, True, Alignment.LEFT))
 
     if check_json_for_key(data['pd3'], 'valuedOverUnit'):
-        table.add(get_table_cell_order_info1("056 - CottonFee                              ",
-                                             "Helvetica", 8, 1, False, False, True, True, Alignment.RIGHT))
-        table.add(get_table_cell_order_info1("", "Helvetica", 2, 1, False, False, True, True, Alignment.RIGHT))
-        table.add(get_table_cell_order_info1(f"${data['pd3']['cottonFee']} {data['pd3']['cottonFeeUnit']}",
-                                             "Helvetica", 3, 1, False, False, True, True, Alignment.LEFT))
-        table.add(get_table_cell_order_info1(f"${data['pd4']['cottonFeeResult']}",
-                                             "Helvetica", 2, 1, False, False, True, True, Alignment.RIGHT))
+        table.add(get_table_cell_order_info("056 - CottonFee                              ",
+                                            "Helvetica", 8, 1, False, False, True, True, Alignment.RIGHT))
+        table.add(get_table_cell_order_info("", "Helvetica", 2, 1, False, False, True, True, Alignment.RIGHT))
+        table.add(get_table_cell_order_info(f"${data['pd3']['cottonFee']} {data['pd3']['cottonFeeUnit']}",
+                                            "Helvetica", 3, 1, False, False, True, True, Alignment.LEFT))
+        table.add(get_table_cell_order_info(f"${data['pd4']['cottonFeeResult']}",
+                                            "Helvetica", 2, 1, False, False, True, True, Alignment.RIGHT))
 
-    table.add(get_table_cell_order_info1(
+    table.add(get_table_cell_order_info(
         "499 - Merchandise Processing Fee", "Helvetica", 8, 1, False, False, True, True, Alignment.RIGHT))
-    table.add(get_table_cell_order_info1("", "Helvetica", 2, 1, False, False, True, True, Alignment.LEFT))
-    table.add(get_table_cell_order_info1("0.3464%", "Helvetica", 3, 1, False, False, True, True, Alignment.LEFT))
-    table.add(get_table_cell_order_info1(f"${data['pd4']['merchandiseProcessingFee']}",
-                                         "Helvetica", 2, 1, False, False, True, True, Alignment.RIGHT))
+    table.add(get_table_cell_order_info("", "Helvetica", 2, 1, False, False, True, True, Alignment.LEFT))
+    table.add(get_table_cell_order_info("0.3464%", "Helvetica", 3, 1, False, False, True, True, Alignment.LEFT))
+    table.add(get_table_cell_order_info(f"${data['pd4']['merchandiseProcessingFee']}",
+                                        "Helvetica", 2, 1, False, False, True, True, Alignment.RIGHT))
 
-    table.add(get_table_cell_order_info1(
+    table.add(get_table_cell_order_info(
         "501 - Harbor Maintenance Fee       ", "Helvetica", 8, 1, False, border_bottom, True, True, Alignment.RIGHT))
-    table.add(get_table_cell_order_info1("", "Helvetica", 2, 1, False, border_bottom, True, True, Alignment.LEFT))
-    table.add(get_table_cell_order_info1("0.1250%",
-                                         "Helvetica", 3, 1, False, border_bottom, True, True, Alignment.LEFT))
-    table.add(get_table_cell_order_info1(f"${data['pd4']['harborMaintenanceFee']}",
-                                         "Helvetica", 2, 1, False, border_bottom, True, True, Alignment.RIGHT))
+    table.add(get_table_cell_order_info("", "Helvetica", 2, 1, False, border_bottom, True, True, Alignment.LEFT))
+    table.add(get_table_cell_order_info("0.1250%",
+                                        "Helvetica", 3, 1, False, border_bottom, True, True, Alignment.LEFT))
+    table.add(get_table_cell_order_info(f"${data['pd4']['harborMaintenanceFee']}",
+                                        "Helvetica", 2, 1, False, border_bottom, True, True, Alignment.RIGHT))
 
     Image(
         Path(f"{root_dir}/../source/line.jpg"),
@@ -195,68 +195,43 @@ def table_total_paragraph_gen(table, j, row_number, is_draw):
 
         for index in range(row_number):
             if index < is_empty or index >= is_empty + 2 and index != row_number - 1:
-                table.add(get_table_cell_order_info1("", "Helvetica", 3, 1, False, False, False, True, Alignment.LEFT))
-                table.add(get_table_cell_order_info1("", "Helvetica", 5, 1, False, False, True, False, Alignment.RIGHT))
-                table.add(get_table_cell_order_info1("", "Helvetica", 2, 1, False, False, True, True, Alignment.RIGHT))
+                table.add(get_table_cell_order_info("", "Helvetica", 3, 1, False, False, False, True, Alignment.LEFT))
+                table.add(get_table_cell_order_info("", "Helvetica", 5, 1, False, False, True, False, Alignment.RIGHT))
+                table.add(get_table_cell_order_info("", "Helvetica", 2, 1, False, False, True, True, Alignment.RIGHT))
                 table.add(
-                    get_table_cell_order_info1("", "Helvetica", 3, 1, False, False, True, True, Alignment.CENTERED))
-                table.add(get_table_cell_order_info1("", "Helvetica", 2, 1, False, False, True, True, Alignment.RIGHT))
+                    get_table_cell_order_info("", "Helvetica", 3, 1, False, False, True, True, Alignment.CENTERED))
+                table.add(get_table_cell_order_info("", "Helvetica", 2, 1, False, False, True, True, Alignment.RIGHT))
             elif index == row_number - 1:
-                table.add(get_table_cell_order_info1("", "Helvetica", 3, 1, False, True, False, True, Alignment.LEFT))
-                table.add(get_table_cell_order_info1("", "Helvetica", 5, 1, False, True, True, False, Alignment.RIGHT))
-                table.add(get_table_cell_order_info1("", "Helvetica", 2, 1, False, True, True, True, Alignment.RIGHT))
+                table.add(get_table_cell_order_info("", "Helvetica", 3, 1, False, True, False, True, Alignment.LEFT))
+                table.add(get_table_cell_order_info("", "Helvetica", 5, 1, False, True, True, False, Alignment.RIGHT))
+                table.add(get_table_cell_order_info("", "Helvetica", 2, 1, False, True, True, True, Alignment.RIGHT))
                 table.add(
-                    get_table_cell_order_info1("", "Helvetica", 3, 1, False, True, True, True, Alignment.CENTERED))
-                table.add(get_table_cell_order_info1("", "Helvetica", 2, 1, False, True, True, True, Alignment.RIGHT))
+                    get_table_cell_order_info("", "Helvetica", 3, 1, False, True, True, True, Alignment.CENTERED))
+                table.add(get_table_cell_order_info("", "Helvetica", 2, 1, False, True, True, True, Alignment.RIGHT))
             else:
-                table.add(get_table_cell_order_info1(
+                table.add(get_table_cell_order_info(
                     f"{title_list[title_list_index][0]}", "Helvetica", 4, 1, False, False, False, True, Alignment.LEFT))
-                table.add(get_table_cell_order_info1(
+                table.add(get_table_cell_order_info(
                     f"{title_list[title_list_index][1]}", "Helvetica", 4, 1, False, False, True, False,
                     Alignment.RIGHT))
-                table.add(get_table_cell_order_info1(
+                table.add(get_table_cell_order_info(
                     f"{title_list[title_list_index][2]}", "Helvetica", 2, 1, False, False, True, True, Alignment.RIGHT))
-                table.add(get_table_cell_order_info1(
+                table.add(get_table_cell_order_info(
                     f"{title_list[title_list_index][3]}", "Helvetica", 3, 1, False, False, True, True,
                     Alignment.CENTERED))
-                table.add(get_table_cell_order_info1(
+                table.add(get_table_cell_order_info(
                     f"{title_list[title_list_index][4]}", "Helvetica", 2, 1, False, False, True, True, Alignment.RIGHT))
                 title_list_index += 1
     else:
         table.add(get_table_cell_order_index("", "Helvetica", 1, row_number, False, True))
 
         for index in range(row_number):
-            table.add(get_table_cell_order_info1("", "Helvetica", 4, 1, False, False, False, True, Alignment.LEFT))
-            table.add(get_table_cell_order_info1("", "Helvetica", 4, 1, False, False, True, False, Alignment.RIGHT))
-            table.add(get_table_cell_order_info1("", "Helvetica", 2, 1, False, False, True, True, Alignment.RIGHT))
+            table.add(get_table_cell_order_info("", "Helvetica", 4, 1, False, False, False, True, Alignment.LEFT))
+            table.add(get_table_cell_order_info("", "Helvetica", 4, 1, False, False, True, False, Alignment.RIGHT))
+            table.add(get_table_cell_order_info("", "Helvetica", 2, 1, False, False, True, True, Alignment.RIGHT))
             table.add(
-                get_table_cell_order_info1("", "Helvetica", 3, 1, False, False, True, True, Alignment.CENTERED))
-            table.add(get_table_cell_order_info1("", "Helvetica", 2, 1, False, False, True, True, Alignment.RIGHT))
-
-
-def get_table_cell_order_info1(value, font, column_span, row_span, border_top, border_bottom, border_right, border_left,
-                               horizontal_alignment) -> TableCell:
-    return TableCell(
-        Paragraph(
-            value,
-            font=font,
-            respect_newlines_in_text=True,
-            respect_spaces_in_text=True,
-            horizontal_alignment=horizontal_alignment,
-            font_size=Decimal(10),
-            padding_top=Decimal(1),
-            padding_left=Decimal(3),
-            padding_right=Decimal(2),
-            fixed_leading=Decimal(0.65),
-        ),
-        border_top=border_top,
-        border_bottom=border_bottom,
-        border_right=border_right,
-        border_left=border_left,
-        border_width=Decimal(0.5),
-        column_span=column_span,
-        row_span=row_span,
-    )
+                get_table_cell_order_info("", "Helvetica", 3, 1, False, False, True, True, Alignment.CENTERED))
+            table.add(get_table_cell_order_info("", "Helvetica", 2, 1, False, False, True, True, Alignment.RIGHT))
 
 
 def draw_line_for_self_style(page):
